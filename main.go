82505c5d2be8acbec95609f1c1b985e10812d264
v310-r00t <agpw.id@gmail.com>
2021-05-07 09:05:52 +0800

    GET doctors & patients

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"doctorsAppointment/handler"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/doctors", handler.GetAllDoctor)
+	r.GET("/patients", handler.GetAllPatient)
+	r.Run(":4444")
 }
