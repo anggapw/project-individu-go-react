307dc221b4f7f4bb9a0edc65830c8af48ec5e4cb
Angga Prasetya W <agpw.id@gmail.com>
2021-05-27 02:09:13 +0800

    fix

--- api/config/db.go
+++ api/config/db.go
@@ -5,20 +5,20 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 func Connection() *gorm.DB {
-	err := godotenv.Load()
+	// err := godotenv.Load()
 
 	dbUser := os.Getenv("DB_USERNAME")
 	dbPass := os.Getenv("DB_PASSWORD")
 	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
 	dbName := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbPort, dbName)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
@@ -26,10 +26,7 @@ func Connection() *gorm.DB {
 	}
 
 	db.AutoMigrate(&entity.User{})
-	db.AutoMigrate(&entity.Doctor{})
-	// db.AutoMigrate(&entity.Category{})
-	// db.AutoMigrate(&entity.Todo{})
-	// db.AutoMigrate(&entity.UserDetail{})
+	// db.AutoMigrate(&entity.Doctor{})
 
 	return db
 }
